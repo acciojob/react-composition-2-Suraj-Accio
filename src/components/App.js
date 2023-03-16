864c5b39a7f6fae081e9c97023627e6b74fc02e7
Suraj-Accio <suraj@acciojob.com>
2023-03-16 12:39:44 +0530

    submitting code-Thu Mar 16 2023 12:39:44 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-
-import React from "react";
+import React, { useState } from "react";
+import Modal from "./Modal";
 import './../styles/App.css';
 
-const App = () => {
+function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <button onClick={handleShowModal}>Show Modal</button>
+      <Modal show={showModal} onClose={handleCloseModal}>
+        <p>This is the content of the modal.</p>
+      </Modal>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/modal.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './../styles/App.css';
+
+function Modal({ show, onClose, children }) {
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal">
+        <button className="modal-close" onClick={onClose}>
+          Close
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
